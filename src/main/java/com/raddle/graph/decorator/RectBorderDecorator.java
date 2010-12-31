84e919f5967bb00669010fd552017ac121eae252
raddle60 <raddle60@e8f522eb-b81d-d541-79ee-739cc54fc1d5>
2010-12-31 09:18:02 +0000

    虚线边框

--- src/main/java/com/raddle/graph/decorator/RectBorderDecorator.java
+++ src/main/java/com/raddle/graph/decorator/RectBorderDecorator.java
@@ -1,10 +1,12 @@
 package com.raddle.graph.decorator;
 
 import java.awt.AlphaComposite;
+import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Composite;
 import java.awt.Graphics2D;
 import java.awt.Rectangle;
+import java.awt.Stroke;
 
 import com.raddle.graph.GraphShape;
 import com.raddle.graph.ShapeDecorator;
@@ -13,6 +15,7 @@ public class RectBorderDecorator implements ShapeDecorator {
 	private Color color = Color.BLACK;
 	private float opacity = 1;
 	private int thickness = 1;
+	private int dashWidth = -1;
 
 	public RectBorderDecorator(Color color, int thickness) {
 		this.color = color;
@@ -23,16 +26,25 @@ public class RectBorderDecorator implements ShapeDecorator {
 	public void decorate(Graphics2D graphics, GraphShape shape) {
 		Color oldColor = graphics.getColor();
 		Composite oldComposite = graphics.getComposite();
+		Stroke oldStroke = graphics.getStroke();
 		graphics.setColor(color);
 		if (opacity > 0 && opacity < 1) {
 			graphics.setComposite(AlphaComposite.SrcOver.derive(opacity));
 		}
 		Rectangle bounds = shape.getBounds();
-		for (int i = 0; i < Math.max(thickness, 1); i++) {
-			graphics.drawRect(bounds.x + i, bounds.y + i, bounds.width - i - i - 1, bounds.height - i - i - 1);
+		if (dashWidth > 0) {
+			graphics.setStroke(new BasicStroke(thickness, BasicStroke.CAP_BUTT, BasicStroke.JOIN_MITER, 1.0f,
+					new float[] { dashWidth }, 0.0f));
+			graphics.draw(new Rectangle(bounds.x + thickness / 2, bounds.y + thickness / 2, bounds.width - thickness,
+					bounds.height - thickness));
+		} else {
+			for (int i = 0; i < Math.max(thickness, 1); i++) {
+				graphics.drawRect(bounds.x + i, bounds.y + i, bounds.width - i - i - 1, bounds.height - i - i - 1);
+			}
 		}
 		graphics.setColor(oldColor);
 		graphics.setComposite(oldComposite);
+		graphics.setStroke(oldStroke);
 	}
 
 	public Color getColor() {
@@ -57,5 +69,13 @@ public class RectBorderDecorator implements ShapeDecorator {
 
 	public void setThickness(int thickness) {
 		this.thickness = thickness;
+	}
+
+	public int getDashWidth() {
+		return dashWidth;
+	}
+
+	public void setDashWidth(int dashWidth) {
+		this.dashWidth = dashWidth;
 	}
 }
